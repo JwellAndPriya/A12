a5a3a7c8c09549bca40abf33b8eaa8a4814dcc06
patelpriya1509 <133623717+patelpriya1509@users.noreply.github.com>
2023-10-08 22:12:37 -0400

    Update DefaultX.cpp

--- DefaultX/DefaultX.cpp
+++ DefaultX/DefaultX.cpp
@@ -1,1 +1,13 @@
+// DefaultX.cpp : Defines the entry point for the application.
+//
+
+#include "DefaultX.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
 
